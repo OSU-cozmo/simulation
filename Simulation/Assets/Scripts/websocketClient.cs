428bc91050b8a04e5421de2e3910a29e2a04fe72
Mason Sidebottom <mason.sidebottom@me.com>
2018-04-28 15:59:22 -0700

    basic JSON client -> server setup & logger added

--- Simulation/Assets/Scripts/websocketClient.cs
+++ Simulation/Assets/Scripts/websocketClient.cs
@@ -8,15 +8,20 @@ public class websocketClient : MonoBehaviour {
 
 	public WebSocket w;
 	public int port = 8765;
+	public List<GameObject> Recievers;
+	int sent = 0;
 	int deltaT = 0;
+	bool done = false;
 	IEnumerator Start() {
 		w = new WebSocket(new Uri(String.Format("ws://localhost:{0}", port)));
 		yield return StartCoroutine(w.Connect());
-		Debug.Log("sending str");
-		w.SendString("handshake successful");
+		string msg = buildInitMessage();
+		Debug.Log(msg);
+		w.SendString(msg);
 		int i = 0;
 		while (true) {
 			string reply = w.RecvString();
+			sent++;
 			if (reply != null) {
 				Debug.Log(reply);
 				w.SendString("Recieved message");
@@ -25,15 +30,73 @@ public class websocketClient : MonoBehaviour {
 			yield return i;
 
 		}
-		w.Close();
-
 	}
 
 	public void Update() {
 		deltaT++;
 		//Send stop command after update happened 300 times
-		if (deltaT > 300) {
-			w.SendString("stop");
+		if (deltaT > 100 && !done) {
+			header stopper = new header();
+			stopper.build("STOP", "Stopping the server", sent);
+			string msg = "JSON{\"header\":" + JsonUtility.ToJson(stopper) + ",\"body\":{}}";
+			Debug.Log(msg);
+			w.SendString(msg);
+			done = true;
 		}
+	}
+
+	private string buildInitMessage() {
+		initMsg msg = new initMsg(this.Recievers);
+		return msg.getJson();
+	}
+}
+
+public class initMsg {
+	public header header;
+	public List<obj> objects;
+	public class obj {
+		public int index;
+		public string name;
+
+		public obj(string name, int index) {
+			this.name = name;
+			this.index = index;
+		}
+	}
+	public initMsg(List<GameObject> obs) {
+		header = new header();
+		header.build("INIT", "initializing world", 0);
+		objects = new List<obj>();
+		for (int i = 0; i < obs.Count; i++) {
+			objects.Add(new obj(obs[i].name, i));
+		}
+	}
+
+	public string getJson() {
+
+		string msg = "JSON{\"header\":";
+		msg += JsonUtility.ToJson(this.header);
+		msg += ",\"body\":{\"objects\":[";
+		for(int i = 0; i < objects.Count; i++) {
+			msg += JsonUtility.ToJson(objects[i]);
+			if (i != objects.Count - 1)
+				msg += ',';
+		}
+		msg += "]}}";
+		return msg;
+	}
+
+}
+
+public class header {
+	public string type, msg;
+	public int seq;
+	public header() { 
+	}
+
+	public void build(string type, string msg, int seq) {
+		this.type = type;
+		this.msg = msg;
+		this.seq = seq;
 	}
 }
--- pyhton/server.py
+++ pyhton/server.py
@@ -1,36 +1,82 @@
 import asyncio
 import websockets
-
+import json
+import logging;
 class server:
 
     outBuf = []
     stop = False
+    lastMsg = 0
+    routes = {}
+    log = -1
     def __init__(self):
-        pass;
+        """Initializes an instance of the robot object."""
+
+        logger = logging.getLogger('SVR')
+        logger.setLevel(logging.DEBUG)
+
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.DEBUG)
+
+        #Logs look like
+        #Robot > ERROR >> This is and Error
+        formatter = logging.Formatter('%(name)s > %(levelname)s >> %(message)s')
+        #using tab to line up all messages
+
+        ch.setFormatter(formatter)
+
+        logger.addHandler(ch)
+        self.log = logger
 
     def start(self):
+        self.log("Server starting\n")
+        self.log.info("Currently Bound Types -> %s" % list(map(lambda x : x, self.routes)))
         _server = websockets.serve(self.handler, 'localhost', 8765)
         asyncio.get_event_loop().run_until_complete(_server)
         asyncio.get_event_loop().run_forever()
 
-
     async def router(self, msg):
-        print (" > %s" % msg)
-        if(msg == "stop"):
-            print("Trying to stop")
-            asyncio.get_event_loop().stop()
-            asyncio.get_event_loop().close();
+
+        if "JSON" not in msg:
+            self.log.warning("Recieved bad message : %s" % msg)
+        else:
+            msg = msg.replace("JSON", "")
+            nmsg = self.msgToDict(msg)
+            if not nmsg:
+                self.log.error("Recieved Bad JSON : %s" % msg)
+                stopClean()
+            else:
+                t = nmsg['header']['type'];
+                self.log.info("Client Message : '%s'" % nmsg['header']['msg'])
+                if t in self.routes:
+                    f = self.routes[t](nmsg['body'])
+                else:
+                    self.log.error("No behavior defined for type %s" % t)
+                    stopClean()
+
+
+
+    def msgToDict(self, msg):
+        try:
+            nmsg = json.loads(msg)
+        except json.JSONDecodeError as e:
+            return False
+        return json.loads(msg)
+
+    def bind(self, key, behavior):
+        self.routes[key] = behavior
 
     async def incoming(self, websocket, path):
         async for msg in websocket:
             await self.router(msg.decode("utf-8"))
 
     async def outgoing(self, websocket, path):
-        print("Sending message")
-        await websocket.send("Hello")
+        msg = "hello"
+        self.log.info("Outgoing : '%s'" % msg)
+        await websocket.send(msg)
 
     async def handler(self, websocket, path):
-        print("connected to %s" % path);
+        self.log.info("connected to new client");
 
         inTask = asyncio.ensure_future(self.incoming(websocket, path))
         outTask = asyncio.ensure_future(self.outgoing(websocket, path))
@@ -41,4 +87,15 @@ class server:
             )
 
 svr = server()
+
+def stopClean(body = None):
+    asyncio.get_event_loop().stop()
+    asyncio.get_event_loop().close()
+
+def sampleInit(body):
+    print("Initializing World State")
+
+svr.bind('STOP', stopClean)
+svr.bind('INIT', sampleInit)
+
 svr.start()
