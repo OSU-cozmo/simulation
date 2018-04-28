1c24cb48191a40e1cbbe6a454f18de529ee460b8
Mason Sidebottom <mason.sidebottom@me.com>
2018-04-27 17:10:36 -0700

    added websocket prototype

---
+++ Simulation/Assets/Scripts/websocketClient.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Net.Sockets;
+
+public class websocketClient : MonoBehaviour {
+
+	public WebSocket w;
+	public int port = 8765;
+	int deltaT = 0;
+	IEnumerator Start() {
+		w = new WebSocket(new Uri(String.Format("ws://localhost:{0}", port)));
+		yield return StartCoroutine(w.Connect());
+		Debug.Log("sending str");
+		w.SendString("handshake successful");
+		int i = 0;
+		while (true) {
+			string reply = w.RecvString();
+			if (reply != null) {
+				Debug.Log(reply);
+				w.SendString("Recieved message");
+			}
+
+			yield return i;
+
+		}
+		w.Close();
+
+	}
+
+	public void Update() {
+		deltaT++;
+		//Send stop command after update happened 300 times
+		if (deltaT > 300) {
+			w.SendString("stop");
+		}
+	}
+}
